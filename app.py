8e3730cc323b2ec2cd5d3101de7d54ebd65f8e97
mdshakilzaman <mdshakilzaman2020@gmail.com>
2022-12-04 19:18:27 -0500

    new pattern

--- app.py
+++ app.py
@@ -1,46 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 db = SQLAlchemy(app)
-db.create_all()
 
+#Database Models
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
-
-new_todo = Todo(title = "todo 1", complete = False)
-db.session.add(new_todo)
-db.session.commit()
+    complete = db.Column(db.Boolean, default=False)
+    
 
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
-    print(todo_list)
-    return render_template('base.html', todo_list = todo_list)
+    total_todo = Todo.query.count()
+    completed_todo = Todo.query.filter_by(complete=True).count()
+    uncompleted = total_todo - completed_todo
+    return render_template('dashboard/index.html', **locals())
 
-@app.route("/add", methods = ["POST"])
+@app.route('/add', methods=['POST'])
 def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete = False)
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
+    db.session.commit() 
+    return redirect(url_for('index'))
 
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
-    todo = Todo.query.filter_by(id = todo_id).first()
+@app.route('/update/<int:id>')
+def update(id):
+    todo = Todo.query.filter_by(id=id).first()
     todo.complete = not todo.complete
     db.session.commit()
-    return redirect(url_for("index"))
+    return redirect(url_for('index'))
 
-@app.route("/delete/<int:todo_id>")
-def delete(todo_id):
-    todo = Todo.query.filter_by(id = todo_id).first()
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.filter_by(id=id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("index"))
+    return redirect(url_for('index'))
